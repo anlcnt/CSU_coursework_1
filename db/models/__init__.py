2ee3b18b1148f14795d69092d61fdb8b5d84f2f4
Anl Cnt <derrechtloz@gmail.com>
2023-02-07 13:40:13 +0300

    alembic

---
+++ db/models/__init__.py
@@ -0,0 +1,8 @@
+from db.models.base import Base, BaseModel
+from db.models.author import Author
+from db.models.book import Book
+from db.models.hall import Hall
+from db.models.field import Field
+from db.models.member import Member
+from db.models.publisher import Publisher
+from db.models.lending import Lending
