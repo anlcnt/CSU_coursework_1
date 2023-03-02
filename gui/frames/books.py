844dfa48690c915969696b1ad0bad0105502d5fd
Anl Cnt <derrechtloz@gmail.com>
2023-03-02 20:01:55 +0300

    books as baseview

--- gui/frames/books.py
+++ gui/frames/books.py
@@ -1,23 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.messagebox import askyesno
 from sqlalchemy import select, update, insert, delete
 from db.models.book import Book
 from db.models.author import Author
 from db.models.publisher import Publisher
 from db.models.field import Field
 from gui.treeviews.books import BooksTree
+from gui.frames.base import BaseView, BaseInputFrame as BIF, error_catcher
 
 
-class BooksInputFrame(tk.Frame):
+class BooksInputFrame(BIF):
     ''' Фрейм ввода данных книги '''
 
     def __init__(self, master, session, handler=None, selected_id=None):
-        super().__init__(master)
-
-        self.session = session
-
         labels = ["Название", "Год издания", "Автор",
-                  "Издатель", "Кол-во страниц", "Полка"]
+                  "Издатель", "Кол-во страниц", "Обл. знаний"]
+
+        super().__init__(master, session, labels, handler, selected_id)
 
         self.authors = self.get_rows(Author)
         self.fields = self.get_rows(Field)
@@ -32,43 +32,46 @@ class BooksInputFrame(tk.Frame):
             self, width=37, values=list(self.publishers.keys()))
         self.field_combobox = ttk.Combobox(
             self, width=37, values=list(self.fields.keys()))
-        self.accept_button = ttk.Button(self, text="Выполнить")
 
-        if handler is not None:
-            self.accept_button.config(
-                command=lambda: handler(self, selected_id))
-
-        def set_grid(e, i):
-            return e.grid(row=i,
-                          column=1,
-                          sticky="e",
-                          padx=(5, 5),
-                          pady=(5, 5))
-
-        set_grid(self.name_entry, 0)
-        set_grid(self.year_entry, 1)
-        set_grid(self.author_combobox, 2)
-        set_grid(self.publisher_combobox, 3)
-        set_grid(self.pages_entry, 4)
-        set_grid(self.field_combobox, 5)
-        set_grid(self.accept_button, 6)
-
-        for i, label in enumerate(labels):
-            ttk.Label(self, text=label).grid(row=i, column=0, sticky="e")
-
-    def get_rows(self, model):
-        query = select(model.name, model.id)
-        result = self.session.execute(query).all()
-        return dict(result)
+        BIF.set_grid(self.name_entry, 0)
+        BIF.set_grid(self.year_entry, 1)
+        BIF.set_grid(self.author_combobox, 2)
+        BIF.set_grid(self.publisher_combobox, 3)
+        BIF.set_grid(self.pages_entry, 4)
+        BIF.set_grid(self.field_combobox, 5)
+        BIF.set_grid(self.accept_button, 6)
 
     def author_id(self):
-        return self.authors[self.author_combobox.get()]
+        author = self.author_combobox.get()
+
+        if author in self.authors.keys():
+            return self.authors[self.author_combobox.get()]
+
+        if askyesno(message="Указанный автор отсутствует. Создать?"):
+            self.create_new_record(Author, name=author)
+            return len(self.authors) + 1
+
+        raise ValueError("Указанный автор отсутствует в базе")
 
     def field_id(self):
-        return self.fields[self.field_combobox.get()]
+        field = self.field_combobox.get()
+
+        if field in self.fields.keys():
+            return self.fields[self.field_combobox.get()]
+
+        raise ValueError("Указанная область знаний отсутствует в базе")
 
     def publisher_id(self):
-        return self.publishers[self.publisher_combobox.get()]
+        publisher = self.publisher_combobox.get()
+
+        if publisher in self.publishers.keys():
+            return self.publishers[self.publisher_combobox.get()]
+
+        if askyesno(message="Указанный издатель отсутствует. Создать?"):
+            self.create_new_record(Publisher, name=publisher)
+            return len(self.publishers) + 1
+
+        raise ValueError("Указанный издатель отсутствует в базе")
 
     def get_data(self):
         return {
@@ -81,12 +84,11 @@ class BooksInputFrame(tk.Frame):
         }
 
 
-class BooksView(tk.Frame):
+class BooksView(BaseView):
     ''' Общий фрейм книг '''
 
     def __init__(self, master, session):
-        super().__init__(master)
-        self.session = session
+        super().__init__(master, session)
 
         # Таблица
         self.treeview = BooksTree(self)
@@ -95,30 +97,8 @@ class BooksView(tk.Frame):
             orient=tk.VERTICAL,
             command=self.treeview.yview)
         self.treeview.configure(yscroll=y_scrollbar.set)
-
-        # Обновление данных когда фрейм видимый
-        # self.bind("<Visibility>", self.update_treeview_data)
+        self.treeview.pack()
         self.update_treeview_data()
-
-        # Кнопки
-        self.add_button = ttk.Button(self, text="Добавить")
-        self.edit_button = ttk.Button(self, text="Изменить")
-        self.remove_button = ttk.Button(self, text="Удалить")
-        self.search_button = ttk.Button(self, text="Поиск")
-        self.update_button = ttk.Button(self, text="Обновить")
-
-        self.add_button.config(command=self.add_data_command)
-        self.edit_button.config(command=self.edit_data_command)
-        self.remove_button.config(command=self.remove_data_command)
-        self.search_button.config(command=self.search_data_command)
-        self.update_button.config(command=self.update_treeview_data)
-
-        self.add_button.grid(row=0, column=0)
-        self.edit_button.grid(row=0, column=1)
-        self.search_button.grid(row=0, column=2)
-        self.remove_button.grid(row=0, column=3)
-        self.update_button.grid(row=0, column=4)
-        self.treeview.grid(row=1, column=0, columnspan=5)
 
     # Обновление данных
     def update_treeview_data(self, event=None):
@@ -154,43 +134,47 @@ class BooksView(tk.Frame):
         frame.pack()
 
     # Команда на удаление
+    @error_catcher
     def remove_data_command(self):
         query = delete(Book).where(Book.id == self.treeview.focus())
         self.session.execute(query)
-        self.update_treeview_data()
 
     # Обработка добавления
+    @error_catcher
     def add_data_handler(self, frame, *args, **kwargs):
         self.session.execute(insert(Book), [frame.get_data()])
-        self.update_treeview_data()
 
     # Обработка поиска
+    @error_catcher
     def search_data_handler(self, frame, *args, **kwargs):
         query = select(Book)
 
         name = frame.name_entry.get()
         year = frame.year_entry.get()
         pages = frame.pages_entry.get()
+        author = frame.author_combobox.get()
+        publisher = frame.publisher_combobox.get()
+        field = frame.field_combobox.get()
 
-        if len(name):
-            query = query.where(Book.name == name)
-        if len(year):
-            query = query.where(Book.year == year)
-        if len(pages):
-            query = query.where(Book.pages == pages)
-        if len(frame.author_combobox.get()):
-            query = query.where(Book.author_id == frame.author_id())
-        if len(frame.publisher_combobox.get()):
-            query = query.where(Book.publisher_id == frame.publisher_id())
-        if len(frame.field_combobox.get()):
-            query = query.where(Book.field_id == frame.field_id())
+        conditions = [
+            (name, Book.name == name),
+            (year, Book.year == year),
+            (pages, Book.pages == pages),
+            (author, Book.author_id == frame.author_id()),
+            (publisher, Book.publisher_id == frame.publisher_id()),
+            (field, Book.field_id == frame.field_id())
+        ]
+
+        for c in conditions:
+            if len(c[0]):
+                query.where(c[1])
 
         result = self.session.execute(query).all()
         self.treeview.set_data(result)
 
     # Обработка редактирования
+    @error_catcher
     def edit_data_handler(self, frame, selected_id, *args, **kwargs):
         query = update(Book).where(Book.id == selected_id)
         query = query.values(**frame.get_data())
         self.session.execute(query)
-        self.update_treeview_data()
